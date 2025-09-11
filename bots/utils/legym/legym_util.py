077345c217a6aae89cea5417d4eb8f17638341a9
jielong.du <1652210516@qq.com>
2025-09-11 18:28:08 +0800

    path

--- bots/utils/legym/legym_util.py
+++ bots/utils/legym/legym_util.py
@@ -281,7 +281,7 @@ class LegymClient:
             _log.info(data)
             data = client.get_limit()
             _log.info(data)
-            with open(pathlib.Path("./utils/legym/map.geojson"), "r", encoding="utf-8") as f:
+            with open(pathlib.Path("./utils/legym/map.geojson").resolve(), "r", encoding="utf-8") as f:
                 content = f.read()
                 data = client.upload(mileage=mileage, end_time=end_time, geojson_str=content)
                 _log.info(data)
