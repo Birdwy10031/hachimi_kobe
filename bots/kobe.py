e16e7ea4152308bc8fc56386167d1a06a83498a2
birdWy <1652210516@qq.com>
2026-02-24 18:03:08 +0800

    sb6657

--- bots/kobe.py
+++ bots/kobe.py
@@ -82,12 +82,12 @@ class MyClient(botpy.Client):
             if cmd == "来点烂梗":
                 search_url_6657 = config["search_url_6657"]
                 random_url_6657 = config["random_url_6657"]
-                data = sb_6657_util.search_meme(url=search_url_6657,keyword=arg,tags="")
-                meme_list = data["list"]
-                _log.info(meme_list)
                 reply = ""
                 if arg:
                     #有参数，搜索
+                    data = sb_6657_util.search_meme(url=search_url_6657, keyword=arg, tags="")
+                    meme_list = data["list"]
+                    _log.info(meme_list)
                     if meme_list:
                         sz = len(meme_list)
                         if sz > 20:
